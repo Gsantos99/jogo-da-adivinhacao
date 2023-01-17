de13b85650bac52c862355390561982866542a27
Giulyano Santos <giulyanofelipesantos@gmail.com>
2023-01-16 23:40:00 -0300

    Fix README e main.js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Jogo-da-adivinhação
+
+## Jogo feito com Html, CSS e javaScript. Feito para exercitar os conceitos de loops e condicionais dentro do javaScript. 
+
+Nesse caso utilizei o While e o condicional IF para estruturar o jogo.
--- main.js
+++ main.js
@@ -13,24 +13,25 @@ function start() {
 
   while (numeroDoUser !== numeroAleatorio) {
     // Pergunta ao usuário um número entre 1 e 10
-    numeroDoUser = Number(
-      prompt(`Adivinhe o número de 1 a 10: \n
-    Número de tentativas: ${tentativas}`)
-    )
+    numeroDoUser = prompt(`Adivinhe o número de 1 a 10: \n
+    Número de tentativas: ${tentativas} \n
+    Número gerado: ${numeroAleatorio}`)
 
     tentativas++ // aumenta o número de tentativas a cada palpite
 
-    if (numeroDoUser === undefined || numeroDoUser === null) {
-      return
-    } // para o código caso o usuário aperte cancelar
+    if (!numeroDoUser) {
+      break
+    } // para o código caso o usuário aperte cancelar ou dê OK e não digite nada!
 
-    if (numeroDoUser === numeroAleatorio) {
-      alert(` Parábens, você acertou o número com ${tentativas} `)
-      return
+    if (numeroDoUser == numeroAleatorio) {
+      alert(` Parábens, você acertou o número com ${tentativas} tentativa(s) `)
     } else {
       alert(
-        `Você errou, digite outro número e tente novamente. Até agora você tentou: ${tentativas} vezes`
+        `Você errou, digite outro número e tente novamente. Até agora você tentou: ${tentativas} vez(es)`
       )
     }
   }
 }
+
+// TODO 1 - Ajeitar o style da página 
+// TODO 2 - Refatorar código do projeto
